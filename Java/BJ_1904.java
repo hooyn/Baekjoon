fbe8afade047be9e447ea6fe265e2858a71466ca
hooyn_n <66655076+hooyn@users.noreply.github.com>
2022-03-02 12:02:05 +0900

    01타일_Silver3 [메모리 초과 시 규칙 찾기]
    모든 경우의 수를 구해서 풀려고 했으나 메모리 초과 오류가 발생했다.
    그래서 1~6정도까지 결과값을 보니까 피보나치 수의 수열처럼 증가하는 것을 발견했다.
    재귀적으로 자기 자신을 불러서 모든 경우의 수를 모두 조사하는 방법보다 규칙을 찾았다면
    그 규칙을 활용하는 것이 더 효율적인 알고리즘이 될 수 있다.

--- Java/BJ_1904.java
+++ Java/BJ_1904.java
@@ -2,47 +2,26 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/**
- * 01타일
- * 00, 1로 만들 수 있는 수
- * N=4일 때는 0011, 0000, 1001, 1100, 1111 등 총 5개
- */
-public class BJ_1904 {
-
-    static String oo = "00";
-    static String l = "1";
-    static int result = 0;
-
+public class BJ_1904_fibo {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        if (n == 1) {
-            result = 1;
-        } else if (n == 2) {
-            result = 2;
+        //결과가 규칙적으로 나온다.
+        //1,2,3,5,8,13,21...피보나치 수의 수열처럼 증가
+
+        if(n==1){
+            System.out.println(1);
+        } else if(n==2){
+            System.out.println(2);
         } else {
-            find(n, "");
-        }
-
-        System.out.println(result);
-    }
-
-    //모든 경우의 수를 다 계산해서 답을 나타내려고 했지만
-    //계속 메모리 초과 오류가 났다.
-    //그래서 다른 방식으로 접근을 해봤다.
-    //결과를 n=1부터 n=5정도 까지 나타내보니까 수가 피보나치 수의 수열처럼 증가하는 것을 파악했다.
-    //다른 알고리즘을 통해서 풀자!
-
-    public static void find(int n, String str){
-        if(str.length()>n){
-            return;
-        } else if(str.length()==n){
-            result = (result+1)%15746;
-            return;
-        } else {
-            find(n, str+"00");
-            find(n, str+"1");
+            int[] ints = new int[n+1];
+            ints[1] = 1;
+            ints[2] = 2;
+            for (int i = 3; i < n+1; i++) {
+                ints[i] = (ints[i-1] + ints[i-2])%15746;
+            }
+            System.out.println(ints[n]);
         }
     }
 }
