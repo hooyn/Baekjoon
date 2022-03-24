7ee2cdbf4a675db3e56bd07f5890103d89181fc8
hooyn_n <66655076+hooyn@users.noreply.github.com>
2022-03-24 12:05:31 +0900

    안테나_Silver3 [배열의 중간 값 찾는 문제]

--- Java/Silver/Greedy/BJ_18310.java
+++ Java/Silver/Greedy/BJ_18310.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /**
@@ -11,38 +12,15 @@ public class BJ_18310 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int house_count = Integer.parseInt(br.readLine());
 
-        int min = 100001;
-        int max = 0;
-
-        int[] house_location = new int[100001];
+        int[] house_location = new int[house_count];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < house_count; i++) {
             int point = Integer.parseInt(st.nextToken());
-
-            if(point<min) min = point;
-            if(point>max) max = point;
-
-            house_location[point]++;
+            house_location[i] = point;
         }
 
-        int criteria = min;
-        int result = 1000000000;
-        int result_idx = 0;
-        while (true) {
-            int sum = 0;
-            for (int i = min; i <= max; i++) {
-                if(house_location[i]!=0){
-                    sum += Math.abs(criteria-i)*house_location[i];
-                }
-            }
-            if(sum<result) {
-                result = sum;
-                result_idx = criteria;
-            }
-            criteria++;
-            if(criteria>max) break;
-        }
+        Arrays.sort(house_location);
 
-        System.out.println(result_idx);
+        System.out.println(house_location[house_count/2-1]);
     }
 }
