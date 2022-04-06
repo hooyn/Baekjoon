0f43f47084f28c6cc0c59f994983a3a6a122471c
hooyn_n <66655076+hooyn@users.noreply.github.com>
2022-04-06 11:18:39 +0900

    촌수계산_다시풀기...

--- Java/Silver/BJ_2644.java
+++ Java/Silver/BJ_2644.java
@@ -1,54 +1,60 @@
-import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
+package baek0406;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
-public class Main {
-	
-	static List<Integer>[] relation;
-	static boolean[] checked;
-	static int res = -1;
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int n = Integer.parseInt(br.readLine());
-		relation = new ArrayList[n+1];
-		checked = new boolean[n+1];
-		for(int i=1; i<n+1; i++) {
-			relation[i] = new ArrayList<>();
-		}
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int x = Integer.parseInt(st.nextToken());
-		int y = Integer.parseInt(st.nextToken());
-		
-		int l = Integer.parseInt(br.readLine());
-		
-		for(int i=0; i<l; i++) {
-			st = new StringTokenizer(br.readLine());
-			int p = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
-			relation[p].add(c);
-			relation[c].add(p);
-		}
-		
-		dfs(x,y, 0);
-		System.out.println(res);
-	}
-	
-	static void dfs(int start, int end, int cnt) {
-		if(start == end) {
-			res = cnt;
-			return; 
-		}
-		
-		checked[start] = true;
-		for(int i=0; i<relation[start].size(); i++) { 
-			int next = relation[start].get(i);
-			if(!checked[next]) {
-				dfs(next, end, cnt+1);
-			}
-		}
-	}
+/**
+ * 촌수계산
+ */
+public class BJ_2644 {
+    static int n;
+    static int a;
+    static int b;
+    static int[][] arr;
+    static boolean[] visited;
+    static int result = -1;
+    static boolean check;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        arr = new int[n+1][n+1];
+        visited = new boolean[n+1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
+
+        int m = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int parent = Integer.parseInt(st.nextToken());
+            int child = Integer.parseInt(st.nextToken());
+
+            arr[parent][child] = 1;
+            arr[child][parent] = 1;
+        }
+        find(a, b, 0);
+
+        System.out.println(result);
+    }
+
+    private static void find(int a, int b, int depth) {
+        if(a==b){
+            result = depth;
+            return;
+        }
+        
+        visited[a] = true;
+        for (int i = 0; i < arr.length; i++) {
+
+        }
+    }
+
+
 }
