e761ffa08a6843a45e1b8af7eeed2933886d8b56
hooyn_n <66655076+hooyn@users.noreply.github.com>
2022-04-14 11:14:32 +0900

    미로 탐색_Silver1 [BFS를 이용한 문제해결]

--- Java/Silver/BFS/BJ_2178.java
+++ Java/Silver/BFS/BJ_2178.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 /**
@@ -9,8 +11,7 @@ import java.util.StringTokenizer;
 public class BJ_2178 {
     static int n;
     static int m;
-    static char[][] arr;
-    static int min = Integer.MAX_VALUE;
+    static int[][] arr;
     static boolean[][] visited;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,36 +20,46 @@ public class BJ_2178 {
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
-        arr = new char[n][m];
+        arr = new int[n][m];
         visited = new boolean[n][m];
 
         for (int i = 0; i < n; i++) {
-            arr[i] = br.readLine().toCharArray();
+            String input = br.readLine();
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = input.charAt(j) - '0';
+
+            }
         } //미로 배열 초기화
 
         visited[0][0] = true;
-        find(0,0, 1);
+        find(0,0);
 
-        System.out.println(min);
+        System.out.println(arr[n-1][m-1]);
     }
 
-    private static void find(int row, int col, int count) {
-        if(row==(n-1)&&col==(m-1)){
-            min = Math.min(min, count);
-        }
-
+    private static void find(int row, int col) {
         int[] d_row = {0, 0, 1, -1};
         int[] d_col = {-1, 1, 0, 0};
 
-        for (int i = 0; i < 4; i++) {
-            int dd_row = row + d_row[i];
-            int dd_col = col + d_col[i];
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[] {row, col});
 
-            if((dd_row>=0&&dd_row<n)&&(dd_col>=0&&dd_col<m)){
-                if(arr[dd_row][dd_col]=='1'&&!visited[dd_row][dd_col]){
-                    visited[row][col] = true;
-                    find(dd_row, dd_col, count+1);
-                    visited[row][col] = false; //백트래킹을 위해 false로 해준다.
+        while(!queue.isEmpty()){
+            int[] poll = queue.poll();
+
+            int queue_row = poll[0];
+            int queue_col = poll[1];
+
+            for (int i = 0; i < 4; i++) {
+                int dd_row = queue_row + d_row[i];
+                int dd_col = queue_col + d_col[i];
+
+                if((dd_row>=0&&dd_row<n)&&(dd_col>=0&&dd_col<m)){
+                    if(arr[dd_row][dd_col]==1&&!visited[dd_row][dd_col]){
+                        queue.add(new int[] {dd_row, dd_col});
+                        arr[dd_row][dd_col] = arr[queue_row][queue_col] + 1;
+                        visited[dd_row][dd_col] = true;
+                    }
                 }
             }
         }
