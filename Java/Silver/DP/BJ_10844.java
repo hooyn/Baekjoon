ceb11fb95ea89ff63fd94044d8e759d59c6987ea
hooyn_n <66655076+hooyn@users.noreply.github.com>
2022-04-20 11:16:07 +0900

    쉬운 계단 수_Silver1 [DP를 이용한 문제해결]

--- Java/Silver/DP/BJ_10844.java
+++ Java/Silver/DP/BJ_10844.java
@@ -1,5 +1,3 @@
-package baek0420;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,37 +7,41 @@ import java.io.InputStreamReader;
  */
 public class BJ_10844 {
     static int n;
-    static int result = 0;
+    static long result = 0;
+    static Long[][] dp;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
-
-        find(0, arr);
-
-        System.out.println(result);
-    }
-
-    private static void find(int depth, int[] arr) {
-        if(depth==n){
-            result++;
-            return;
-        }
+        dp = new Long[n+1][10];
 
         for (int i = 0; i < 10; i++) {
-            if(depth==0&&i==0){
-                continue;
-            }
+             dp[1][i] = 1L;
+        }
 
-            arr[depth] = i;
+        for (int i = 1; i < 10; i++) {
+            result += find(n, i);
+        }
 
-            if(depth!=0){
-                if(Math.abs(arr[depth-1]-arr[depth])==1){
-                    find(depth+1, arr);
-                }
-            } else if(depth==0){
-                find(depth+1, arr);
+        for (Long[] longs : dp) {
+        System.out.println(result % 1000000000);
+    }
+
+    private static long find(int digit, int val) {
+        if(digit==1){
+            return dp[digit][val];
+        }
+
+        if(dp[digit][val] == null){
+            if(val == 0){
+                dp[digit][val] = find(digit-1, 1);
+            } //val가 0일 경우 1밖에 들어올 수 없다.
+            else if(val == 9){
+                dp[digit][val] = find(digit-1, 8);
+            } //val가 9일 경우 다음은 8밖에 올 수 없다.
+            else{
+                dp[digit][val] = find(digit-1, val-1) + find(digit-1, val+1);
             }
         }
+        return dp[digit][val] % 1000000000;
     }
 }
