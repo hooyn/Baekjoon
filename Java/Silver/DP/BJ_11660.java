0f3267852a4f4c1640698328515420f6f15ff838
hooyn_n <66655076+hooyn@users.noreply.github.com>
2022-04-28 08:31:45 +0900

    구간 합 구하기 5_Silver1 [DP를 이용한 문제해결]

--- Java/Silver/DP/BJ_11660.java
+++ Java/Silver/DP/BJ_11660.java
@@ -15,6 +15,7 @@ public class BJ_11660 {
         int m = Integer.parseInt(st.nextToken());
 
         int[][] arr = new int[n+1][n+1];
+        int[][] dp = new int[n+1][n+1];
 
         for (int i = 1; i <= n; i++) {
             st = new StringTokenizer(br.readLine());
@@ -23,24 +24,22 @@ public class BJ_11660 {
             }
         }
 
-        for (int i = 0; i < m; i++) {
-            long sum = 0L;
-            st = new StringTokenizer(br.readLine());
-
-            int col1 = Integer.parseInt(st.nextToken());
-            int row1 = Integer.parseInt(st.nextToken());
-
-            int col2 = Integer.parseInt(st.nextToken());
-            int row2 = Integer.parseInt(st.nextToken());
-
-            for (int j = col1; j <= col2; j++) {
-                for (int k = row1; k <= row2; k++) {
-                    int num = arr[j][k];
-                    sum += num;
-                }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1] + arr[i][j];
             }
+        } //배열에서 i, j 까지의 누적합을 구하는 DP 점화식
 
-            System.out.println(sum);
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            System.out.println(dp[x2][y2] - dp[x2][y1-1] - dp[x1-1][y2] + dp[x1-1][y1-1]);
         }
     }
 }
+
+//참고: https://subbak2.tistory.com/65
