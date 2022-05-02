e0f5301a87ddd6288b913bd908888ca6ebf07042
hooyn_n <66655076+hooyn@users.noreply.github.com>
2022-05-03 08:22:47 +0900

    카드 구매하기 2_Silver1 [DP를 이용한 문제해결]

---
+++ Java/Silver/DP/BJ_16194.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 카드 구매하기 2
+ */
+public class BJ_16194 {
+    static int n;
+    static int[] arr;
+    static int[] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        arr = new int[n+1];
+        dp = new int[n+1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 1; i <= n; i++) {
+            dp[i] = Integer.MAX_VALUE;
+        }
+
+        find(n);
+        System.out.println(dp[n]);
+    }
+
+
+    private static void find(int num) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i-j] + arr[j]);
+
+            }
+
+        }
+    }
+}
