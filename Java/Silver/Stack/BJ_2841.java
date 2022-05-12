c6056e227ddc32bb96beaa00a888d54201a0fc6e
hooyn_n <66655076+hooyn@users.noreply.github.com>
2022-05-12 13:57:46 +0900

    외계인의 기타 연주_Silver1 [배열을 스택으로 활용한 문제풀이]

---
+++ Java/Silver/Stack/BJ_2841.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 외계인의 기타 연주
+ */
+public class BJ_2841 {
+    static int N;
+    static int P;
+    static int[][] arr;
+    static int count = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        P = Integer.parseInt(st.nextToken());
+
+        int[] idx = new int[7];
+        arr = new int[7][N];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int p = Integer.parseInt(st.nextToken());
+
+            if(idx[n]==0){
+                arr[n][idx[n]] = p;
+                idx[n]++;
+                count++;
+            } else {
+                while (true) {
+                    int x = arr[n][idx[n]-1]; //전에 들어가 있던 수
+                    if(x==p){
+                        break;
+                    } else if(x<p){
+                        arr[n][idx[n]] = p;
+                        idx[n]++;
+                        count++;
+                        break;
+                    } else if(x>p){
+                        idx[n]--;
+                        count++;
+                    }
+
+                    if(idx[n]==0){
+                        arr[n][idx[n]] = p;
+                        idx[n]++;
+                        count++;
+                        break;
+                    }
+                }
+            }
+        }
+
+        System.out.println(count);
+    }
+}
