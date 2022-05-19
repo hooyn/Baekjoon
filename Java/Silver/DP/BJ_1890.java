b5255e732cf6334473619bc22806eba6e2c13120
hooyn_n <66655076+hooyn@users.noreply.github.com>
2022-05-19 09:34:13 +0900

    점프_Silver2 [DP를 이용한 문제해결]

--- Java/Silver/DP/BJ_1890.java
+++ Java/Silver/DP/BJ_1890.java
@@ -29,16 +29,16 @@ public class BJ_1890 {
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if(i==n-1&&j==n-1) break;
+                if(i==n-1&&j==n-1) break; //지점에 도착했을 때
 
-                if(dp[i][j]!=0){
-                    int plus = arr[i][j];
+                if(dp[i][j]!=0){ //오른쪽하고 아래로만 점프하기 때문에 차례대로 이동하여 점프를 할 수 있었다면 dp[i][j]는 0보다 클 것이다.
+                    int plus = arr[i][j]; //증감값 저장
 
-                    if(i+plus<n){
-                        dp[i+plus][j] += dp[i][j];
+                    if(i+plus<n){ //범위 내에 위치해 있다면
+                        dp[i+plus][j] += dp[i][j]; //dp[i][j](해당 i,j까지의 경우의 수)를 점프한 위치에 더하여 저장한다.
                     }
-                    if(j+plus<n){
-                        dp[i][j+plus] += dp[i][j];
+                    if(j+plus<n){ //범위 내에 위치해 있다면
+                        dp[i][j+plus] += dp[i][j]; //dp[i][j](해당 i,j까지의 경우의 수)를 점프한 위치에 더하여 저장한다.
                     }
                 }
             }
