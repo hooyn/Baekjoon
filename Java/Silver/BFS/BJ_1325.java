7afa1fe5cb61e108984f0b6a163398499a487484
hooyn_n <66655076+hooyn@users.noreply.github.com>
2022-05-21 11:42:22 +0900

    효율적인 해킹_Silver1 [BFS를 이용한 문제해결]
    List<Integer>[] list = new ArrayList[n+1];
            for (int i = 1; i <= n; i++) {
                list[i] = new ArrayList<>();
            }
    
    리스트 배열 선언과 DFS대신 BFS를 이용한 문제해결

--- Java/Silver/BFS/BJ_1325.java
+++ Java/Silver/BFS/BJ_1325.java
@@ -1,18 +1,17 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 /**
  * 효율적인 해킹
  */
 public class BJ_1325 {
-    static boolean[][] map;
-    static boolean[] visited;
+    static List<Integer>[] list;
+    static int[] visited;
     static int dp[];
     static int n;
     static int m;
-    static int max = 0;
     static int pi(String s){return Integer.parseInt(s);}
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,44 +20,67 @@ public class BJ_1325 {
         n = pi(st.nextToken());
         m = pi(st.nextToken());
 
+        visited = new int[n+1];
         dp = new int[n+1];
-        map = new boolean[n+1][n+1];
 
+        list = new ArrayList[n+1];
+        for (int i = 1; i <= n; i++) {
+            list[i] = new ArrayList<>();
+        }
 
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
-            int to = pi(st.nextToken());
-            int from = pi(st.nextToken());
 
-            map[from][to] = true;
+            int x = pi(st.nextToken());
+            int y = pi(st.nextToken());
+
+            list[x].add(y);
         }
 
         for (int i = 1; i <= n; i++) {
-            visited = new boolean[n+1];
-            find(i, 1, i);
+            visited = new int[n+1];
+            find_optimize(i);
         }
 
-        for (int i = 0; i < dp.length; i++) {
+        int max = 0;
+        for (int i = 1; i <= n; i++) {
+             if(dp[i] > max) max = dp[i];
+        }
+
+        for (int i = 1; i <= n; i++) {
             if(dp[i] == max){
                 System.out.print(i + " ");
             }
         }
     }
 
-    private static void find(int depth, int count, int start) {
-        boolean check = false;
-        visited[depth] = true;
-
-        for (int i = 1; i <= n; i++) {
-            if(map[depth][i]==true && !visited[i]){
-                find(i, count+1, start);
-                check = true;
+    private static void find(int node) {
+        for (int i = 0; i < list[node].size(); i++) {
+            Integer integer = list[node].get(i);
+            if(visited[integer] == 0){
+                dp[integer]++;
+                visited[integer] = 1;
+                find(integer);
             }
         }
 
-        if(!check){
-            dp[start] = Math.max(dp[start], count);
-            max = Math.max(max, count);
+    }
+
+    private static void find_optimize(int node) {
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.add(node);
+        visited[node] = 1;
+        while (!queue.isEmpty()) {
+            node = queue.remove();
+            for (Integer integer : list[node]) {
+                if(visited[integer]==0){
+                    dp[integer]++;
+                    visited[integer] = 1;
+                    queue.add(integer);
+                }
+            }
         }
+
     }
 }
