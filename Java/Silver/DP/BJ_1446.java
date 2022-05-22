430fc0d3a4ab7981f8c469bf55e82b20b2713f7e
hooyn_n <66655076+hooyn@users.noreply.github.com>
2022-05-22 23:24:51 +0900

    지름길_Silver1 [다익스트라를 이용한 문제해결]
    지름길의 시작위치에서 로직 실행 시켰을 때 -> 실패
    지름길의 끝 위치에서 로직 실행 시켰을 때 -> 성공

--- Java/Silver/DP/BJ_1446.java
+++ Java/Silver/DP/BJ_1446.java
@@ -6,7 +6,7 @@ import java.util.*;
 /**
  * 지름길
  */
-public class BJ_1446_dijkstra {
+public class BJ_1446 {
     static class Road{
         int start, end, len;
 
@@ -14,15 +14,6 @@ public class BJ_1446_dijkstra {
             this.start = start;
             this.end = end;
             this.len = len;
-        }
-
-        @Override
-        public String toString() {
-            return "Road{" +
-                    "start=" + start +
-                    ", end=" + end +
-                    ", len=" + len +
-                    '}';
         }
     }
     static int pi(String s){return Integer.parseInt(s);}
@@ -50,29 +41,20 @@ public class BJ_1446_dijkstra {
         Collections.sort(list, new Comparator<Road>() {
             @Override
             public int compare(Road o1, Road o2) {
-                if(o1.start==o2.start) {
-                    return o1.end - o2.end;
-                }
-                else{
-                    return o1.start-o2.start;
-                }
+                return o1.end - o2.end;
             }
         });
-
-        for (Road road : list) {
-            System.out.println(road);
-        }
 
         int idx = 0, move = 0;
         int[] dist = new int[10001];
         Arrays.fill(dist, 10001); //배열 값 전체 10001(최대값)로 채우기
         dist[0] = 0;
 
-        while(move<d){
-            if(idx < list.size()){ //list에 들어가 있는 값이 없다면
+        while(move<=d){
+            if(idx < list.size()){ //list에 들어가 있는 값이 있다면
                 Road road = list.get(idx); //저장한 값중 하나 빼고
-                if(move == road.start){ //만약 현재 위치가 지름길의 시작위치이면
-                    dist[road.end] = Math.min(dist[move] + road.len, dist[road.end]); //더 빨리 갈 수 있다면
+                if(move == road.end){ //만약 현재 위치가 지름길의 끝 위치이면
+                    dist[road.end] = Math.min(dist[road.start] + road.len, dist[road.end]); //지름길 시작위치 + 지름길 길이랑 현재 위치의 움직인 move비교
                     idx++; //list 다음 값으로 이동
                 } else {
                     dist[move+1] = Math.min(dist[move+1], dist[move] + 1);
