5db9466eef271348aee46c71f78b7b0cc23f690b
hooyn_n <66655076+hooyn@users.noreply.github.com>
2022-06-05 08:28:10 +0900

    직사각형_Silver1

--- Java/Silver/기하학/BJ_2527.java
+++ Java/Silver/기하학/BJ_2527.java
@@ -16,22 +16,23 @@ public class BJ_2527 {
 
             int x1 = Integer.parseInt(st.nextToken());
             int y1 = Integer.parseInt(st.nextToken());
+            int p1 = Integer.parseInt(st.nextToken());
+            int q1 = Integer.parseInt(st.nextToken());
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
+            int p2 = Integer.parseInt(st.nextToken());
+            int q2 = Integer.parseInt(st.nextToken());
 
-            int x3 = Integer.parseInt(st.nextToken());
-            int y3 = Integer.parseInt(st.nextToken());
-            int x4 = Integer.parseInt(st.nextToken());
-            int y4 = Integer.parseInt(st.nextToken());
-
-
-            if((x2 == x3 && y2 == y3) || (x1 == x4 && y2 == y3) || (x2 == x3 && y1 == y4) || (x1 == x4 && y1 == y4)){
-                System.out.println("c");
-            } else if((x2 == x3 && y2 != y3) || (x1 == x4 && y2 != y3) || (x2 != x3 && y1 == y4) || (x1 != x4 && y1 == y4)){
-                System.out.println("b");
-            } else if(x2 < x3 || x4 < x1 || y2 < y3 || y4 < y1) {
+            if (p1 < x2 || q1 < y2 || p2 < x1 || q2 < y1) {
                 System.out.println("d");
-            } else {
+            }
+            else if ((x1 == p2 && y1 == q2) || (x1 == p2 && q1 == y2) || (p1 == x2 && q1 == y2) || (p1 == x2 && y1 == q2)) {
+                System.out.println("c");
+            }
+            else if (p1 == x2 || q1 == y2|| p2 == x1 || y1 == q2){
+                System.out.println("b");
+            }
+            else {
                 System.out.println("a");
             }
         }
