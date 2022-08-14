9850f7ca26ef7dc96ee5c5e8e842e1d2e255de39
hooyn_n <66655076+hooyn@users.noreply.github.com>
2022-08-14 18:28:50 +0900

    알람 시계_Bronze 3

---
+++ Java/Bronze/구현/BJ_2884.java
@@ -0,0 +1,30 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 알람 시계
+ */
+public class BJ_2884 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int h = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        if(m<45){
+            h--;
+            m = 60 - (45-m);
+            if(h<0){
+                h=23;
+            }
+            System.out.println(h + " " + m);
+        } else {
+            System.out.println(h + " " + (m-45));
+        }
+    }
+}
