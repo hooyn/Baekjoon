2ac544a1fa583aaf649a60b3194ab312185ec4b8
hooyn_n <66655076+hooyn@users.noreply.github.com>
2022-08-20 09:53:36 +0900

    최소, 최대_Bronze 3

---
+++ Java/Bronze/구현/BJ_10818.java
@@ -0,0 +1,29 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/*
+ * 최소, 최대
+ */
+public class BJ_10818 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int max = 0;
+        int min = Integer.MAX_VALUE;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        System.out.println(arr[0] + " " + arr[n-1]);
+    }
+}
